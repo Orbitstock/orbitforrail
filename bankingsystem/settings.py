2e25ac19d95437670e49dca1ba4e6c9a0542f37c
Orbitstock <orbitstockindex@gmail.com>
2025-12-25 07:26:39 +0100

    Update settings.py

--- bankingsystem/settings.py
+++ bankingsystem/settings.py
@@ -16,7 +16,7 @@ os.environ['SSL_CERT_FILE'] = certifi.where()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://web-production-b6be8.up.railway.app",
+    "https://Star-fx-investment.up.railway.app",
     "https://www.brightstarfinancefx.com"
 ]
 
